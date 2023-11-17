31a56f832676bbeb1263811e4ddfcf4c82d92548
Chris Manson <chris@manson.ie>
2023-11-17 16:22:40 +0000

    load markdown dynamically

--- src/main.js
+++ src/main.js
@@ -6,6 +6,29 @@ import Notes from 'reveal.js/plugin/notes/notes.esm.js';
 import 'reveal.js/dist/reveal.css';
 import './mainmatter.css';
 
+const markdownFiles = import.meta.glob('./../slides/*.md', { as: 'raw', eager: true });
+
+const sortedMarkdownFiles = Object.entries(markdownFiles).sort(([a], [b]) => {
+	const regex = /slides\/(\d+)-/;
+	const [, aNumb] = regex.exec(a);
+	const [, bNumb] = regex.exec(b);
+
+	return Number(aNumb) - Number(bNumb);
+});
+
+const sections = sortedMarkdownFiles.map(([, content]) => `
+	<section 
+		data-markdown
+		data-separator="^\n___\n$"
+		data-separator-vertical="^\n---\n$"
+		data-separator-notes="^Note:"
+	>
+		<textarea data-template>${content}</textarea>
+	</section >
+`);
+
+document.querySelector('.slides').innerHTML = sections.join('');
+
 const deck = new Reveal();
 
 deck.initialize({
